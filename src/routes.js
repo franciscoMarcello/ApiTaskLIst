aeaa61ff87e8f3a72d38cd66d3e0c87d19d0a74a
Francisco Marcello <fmarcello265@gmail.com>
2021-11-30 23:07:12 -0400

    apiTask

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleawre from './app/middlewares/auth';
+import TaskController from './app/controllers/TaskController';
+
+const routes = new Router();
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+// Precisa esta autenticado
+routes.use(authMiddleawre);
+routes.put('/users', UserController.update);
+routes.get('/tasks', TaskController.index);
+routes.post('/tasks', TaskController.store);
+routes.put('/tasks/:task_id', TaskController.update);
+routes.delete('/tasks/:task_id', TaskController.delete);
+
+export default routes;
